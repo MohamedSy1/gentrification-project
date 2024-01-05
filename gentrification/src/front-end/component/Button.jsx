89fe318b8d17f40c7cf94ae378ee84e1a6689b9d
MohamedSy1 <mohamed.sy@compscihigh.org>
2024-01-05 18:29:33 -0500

    Completed the map and displays locations and percent increase

--- gentrification/src/front-end/component/Button.jsx
+++ gentrification/src/front-end/component/Button.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import 'leaflet/dist/leaflet.css'
 import datas from "../../backend/json_files/real_output.json"
-import { useState } from 'react'
 
 // I want to create a button component that will let the user select the year.
 // I will map through the data. and create options that will let me select the year
 // I need two select option that will let me select the two year
-// 
-
 // when i select two years I need to map through the data
 // for each object I need to match the key with the selected year
 // I will take that year-value and compare it to the other years value
@@ -15,32 +12,24 @@ import { useState } from 'react'
 // if the year is greater but not double greater the year I would need to make the color yellow 
 // else I need to keep the color green
 
-
-const years = ["2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021", "2023"]
-
-
-
-
 export function MyButton({startYear, setStartYear, endYear, setEndYear}){
-
+    const years = []
+    for (let i = 2010; i <= 2023; i++) {
+        years.push(<option value={i}>{i}</option>)
+    }
     const handleStartChange = (event) => {
         setStartYear(event.target.value)
     }
     const handleEndChange = (event) => {
         setEndYear(event.target.value)
     }
-
     return (
         <div>
-            <select value={startYear} className='SelectingYear' onChange={handleStartChange}>
-                {years.map((year) => (
-                    <option key={Math.random()} value={year}>{year}</option>
-                ))}
+            <select value={startYear} className='SelectingYear' key={Math.random() * 1000} onChange={handleStartChange}>
+                {years}
             </select>
-            <select value={endYear} className='SelectingYear' onChange={handleEndChange}>
-                {years.map((year) => (
-                    <option key={Math.random()} value={year}>{year}</option>
-                ))}
+            <select value={endYear} className='SelectingYear' key={Math.random() * 1000} onChange={handleEndChange}>
+                {years}
             </select>
         </div>
     )

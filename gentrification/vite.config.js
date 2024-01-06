8b6b9e93b1c000005d4a84b2f0d5172f8fad7f4d
MohamedSy1 <mohamed.sy@compscihigh.org>
2024-01-06 14:13:46 -0500

    temp

--- gentrification/vite.config.js
+++ gentrification/vite.config.js
@@ -5,5 +5,6 @@ export default defineConfig({
   // so just run npm build before pushing to GitHub and this will rebuild our assets to the root
   build: { outDir: '..' },
   // needed for github pages just put the repo name here
-  base: '/your-repo-name-here/', 
+  base: '/gentrification-project/', 
+  plugins: [react()]
 });
